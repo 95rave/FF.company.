2986766a36f71eadfeaa56a5621c8c67bc5b308a
95rave <ravanaap@code.edu.az>
2023-05-13 21:39:06 +0400

    Interface

---
+++ Company.Departament/Interface/IDepartament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Departament.Interface1
+{
+    public interface IDepartament: IEntity
+    {
+        int EmployeeLimit { get; set; }
+        int CompanyId { get; set; }
+        void AddEmployee(IEmployee employee);
+        void UpdateDepartment(string newName, int newEmployeeLimit);
+        void GetDepartmentEmployees();
+    }
+}
---
+++ Company.Departament/Interface/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Departament.Interface1
+{
+    public interface IEmployee:IEntity
+    {
+        decimal Salary { get; set; }
+        string Surname { get; set; }
+        int DepartmentId { get; set; }
+    }
+}
