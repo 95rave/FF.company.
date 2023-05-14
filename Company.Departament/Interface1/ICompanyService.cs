ddc0ff1ef3d9c8e30dc2b83a53901580d680c93c
95rave <ravanaap@code.edu.az>
2023-05-14 21:29:20 +0400

    Service Interface add

---
+++ Company.Departament/Interface1/ICompanyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Departament.Interface1
+{
+    public interface ICompanyService
+    {
+        ICompany AddCompany(string name);
+        List<ICompany> GetAllCompanies();
+        ICompany GetCompanyById(int companyId);
+        void UpdateCompanyName(int companyId, string newName);
+        void RemoveCompany(int companyId);
+    }
+}
